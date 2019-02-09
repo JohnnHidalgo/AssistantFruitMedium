44373602cad0c04158ddd06ed50eec86e02fe97f
Johnn Hidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-02-09 09:40:39 -0400

    Create index.js
    Archivo index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Importe el módulo Dialogflow desde la biblioteca de acciones en el cliente de Google.
+const {dialogflow} = require('actions-on-google');
+
+// Importe el firebase-functions package para despliegue.
+const functions = require('firebase-functions');
+
+// Instancie el cliente de Dialogflow.
+const app = dialogflow({debug: true});
+
+// Handle the Dialogflow intent named 'favorite color'.
+// La intención recoge un parámetro llamado 'Fruta'.
+app.intent('Fruta Compra', (conv, {Fruta}) => {
+    const presio = Fruta.length;
+    // Responde con el presio y finaliza la conversación.
+    conv.close('El precio es ' + presio);
+});
+
+// Establecer el objeto DialogflowApp para manejar la solicitud POST de HTTPS.
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
